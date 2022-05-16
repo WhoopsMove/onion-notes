aef44cb41c293449c7cdf309bfce522339b8e7f9
Jami <qiqhsqwmo@mozmail.com>
2022-05-16 19:16:42 +0300

    Added a command loop to change message and quit program as well as added a simple template to render the message

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import os
 import shutil
 import sys
+import asyncio
+import signal
 from flask import Flask
 from flask import g
+from threading import Thread
 
 from tor import Tor
+from web import Web
 
-app = Flask(__name__)
+def command_loop(web_obj: Web):
+    while True:
+        c = input("Enter command ('help' for a list of commands)\n")
+        if c == 'q':
+            raise KeyboardInterrupt
+        elif c == 'u':
+            msg = input("Enter message: ")
+            web_obj.set_msg(msg)
+        elif c == "help":
+            print("Commands:\n---'q' to quit\n---'u' to update message that is being shared")
 
-message = input("Enter message: ")
-
-@app.route('/')
-def index():
-    return get_message()
-
-def get_message():
-    if 'message' not in g:
-        g.message = f"<p>{message}</p>"
-    return g.message
 
 def main():
     tor = Tor()
     tor.connect()
     tor.create_hidden_service()
     try:
-        app.run()
+        web = Web()
+        t = Thread(target=web.run, daemon=True)
+        t.start()
+        command_loop(web)
     except KeyboardInterrupt:
         return
     finally:
         tor.remove_hidden_service()
-
 
 if __name__ == "__main__":
     main()
--- web.py
+++ web.py
@@ -1,5 +1,38 @@
+import asyncio
+from flask import (Flask, g, render_template)
+
 class Web:
     def __init__(self):
-        pass
+        self.message = None
+        self.app = Flask(__name__)
+        self.define_routes()
 
-    
+    security_headers = [
+            ("X-Frame-Options", "DENY"),
+            ("X-Xss-Protection", "1; mode=block"),
+            ("X-Content-Type-Options", "nosniff"),
+            ("Referrer-Policy", "no-referrer"),
+            ("Server", "Onion-Notes"),
+        ]
+
+    def define_routes(self):
+        @self.app.after_request
+        def add_security_headers(res):
+            default_csp = "default-src 'self'; frame-ancestors 'none'; form-action 'self'; base-uri 'self'; img-src 'self' data:;"
+            for header, value in self.security_headers:
+                res.headers.set(header, value)
+            res.headers.set("Content-Security-Policy", default_csp)
+            return res
+
+        @self.app.route('/')
+        def index():
+            return render_template("default.html", message=self.get_message())
+
+    def get_message(self):
+        return self.message
+
+    def run(self):
+        self.app.run()
+
+    def set_msg(self, msg):
+        self.message = msg
