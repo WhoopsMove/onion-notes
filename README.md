cc5b3c7a0f867de878013a886d575da08f91094a
Jami <qiqhsqwmo@mozmail.com>
2022-05-16 19:16:32 +0300

    Updated readme with some instructions

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # onion-notes
+
+## Requirements
+``
+python3, 
+tor
+``
+
+## Install required dependencies
+
+```pip install pynacl cepa flask```
+
+## Tor Configuration
+
+First, you need to locate the torrc file. By default it is generally located in /etc/tor folder on Linux. If running Tor via commandline, the file location will also be displayed there when tor is being started.
+
+Once you have located the file, uncomment the following lines by removing the "#" character:
+
+``#ControlPort 9051``
+``#CookieAuthentication 1``
--- tor.py
+++ tor.py
@@ -21,12 +21,12 @@ class Tor:
 
     def create_hidden_service(self):
         (auth_pubkey, auth_privkey) = self.get_new_auth_keys()
-        result = self.controller.create_ephemeral_hidden_service({80: 5000}, await_publication=True, client_auth_v3=auth_pubkey)
+        result = self.controller.create_ephemeral_hidden_service({80: 5000}, await_publication=True, client_auth_v3=self.key_str(auth_pubkey))
 
         if result.service_id:
             self.tmp_service_id = result.service_id
             print(" * Our service is available at %s.onion, press ctrl+c to quit" % self.tmp_service_id)
-            print(f"Private key: {auth_privkey}")
+            print(f"Private key: {self.key_str(auth_privkey)}")
         else:
             print(" * Unable to determine our service's hostname, probably due to being unable to read the hidden service directory")
 
@@ -40,7 +40,7 @@ class Tor:
         return (pub_key, priv_key)
 
     # https://github.com/pastly/python-snippits/blob/master/src/tor/x25519-gen.py
-    def key_str(key):
+    def key_str(self, key):
         # bytes to base 32
         key_bytes = bytes(key)
         key_b32 = base64.b32encode(key_bytes)
